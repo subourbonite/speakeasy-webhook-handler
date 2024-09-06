cf981924ecc841418caea8a778b987459cca8043
Subourbonite <wrbriggs@gmail.com>
2024-09-05 23:11:47 -0400

    Messing around with Spring Boot serverless

--- src/test/java/cloud/subourbonite/speakeasy/DiscordHashValidatorTest.java
+++ src/test/java/cloud/subourbonite/speakeasy/DiscordHashValidatorTest.java
@@ -24,16 +24,43 @@ class DiscordHashValidatorTest {
     KeyPair keyPair = Crypto.signingKeyPair();
 
     @Test
-    void demoTestMethod() {
+    void testSignatureValidation() {
         DiscordHashValidator validator = new DiscordHashValidator("", "",
                 HexFormat.of().formatHex(keyPair.getPublic().getEncoded()));
 
         Long timestamp = new Date().getTime();
 
+        String body = "{\r\n" + //
+                "    \"app_permissions\": \"562949953601536\",\r\n" + //
+                "    \"application_id\": \"1276733792542916608\",\r\n" + //
+                "    \"authorizing_integration_owners\": {},\r\n" + //
+                "    \"entitlements\": [],\r\n" + //
+                "    \"id\": \"1281449795176628325\",\r\n" + //
+                "    \"token\": \"aW50ZXJhY3Rpb246MTI4MTQ0OTc5NTE3NjYyODMyNTprSTJXNG9Rd0FSQ0hQbGxBRDJLSFA0V2s1TjRJbjhKa1plUjQ0T1F2aFd4MEVybmdWT1cyNnc4NFJLcllCR1NGcFVDbG9VQjZhWnBuZHNZeVE3V0g1cGZhTThlM3lkaHhScDVnOW5jeTlXd0hMd0puRTRQNUxlaUQ3ak9TRGt2cQ\",\r\n"
+                + //
+                "    \"type\": 1,\r\n" + //
+                "    \"user\": {\r\n" + //
+                "        \"avatar\": \"c6a249645d46209f337279cd2ca998c7\",\r\n" + //
+                "        \"avatar_decoration_data\": null,\r\n" + //
+                "        \"bot\": true,\r\n" + //
+                "        \"clan\": null,\r\n" + //
+                "        \"discriminator\": \"0000\",\r\n" + //
+                "        \"global_name\": \"Discord\",\r\n" + //
+                "        \"id\": \"643945264868098049\",\r\n" + //
+                "        \"public_flags\": 1,\r\n" + //
+                "        \"system\": true,\r\n" + //
+                "        \"username\": \"discord\"\r\n" + //
+                "    },\r\n" + //
+                "    \"version\": 1\r\n" + //
+                "}";
+
         APIGatewayV2HTTPResponse response = validator.handleRequest(
                 APIGatewayV2HTTPEvent.builder()
-                        .withBody("{ \"type\": 1 }")
-                        .withHeaders(Map.of("x-signature-timestamp", timestamp.toString(), "x-signature-ed25519", HexFormat.of().formatHex(Crypto.sign(keyPair.getPrivate(), (timestamp.toString() + "{ \"type\": 1 }").getBytes()))))
+                        .withBody(body)
+                        .withHeaders(Map.of("x-signature-timestamp", timestamp.toString(), "x-signature-ed25519",
+                                HexFormat.of()
+                                        .formatHex(Crypto.sign(keyPair.getPrivate(),
+                                                (timestamp.toString() + body).getBytes()))))
                         .build(),
                 new Context() {
                     @Override
@@ -113,6 +140,7 @@ class DiscordHashValidatorTest {
                     }
                 });
 
-        assertEquals(response.getStatusCode(), 200);
+        assertEquals(200, response.getStatusCode());
+        assertEquals(response.getBody(), "{ \"type\": 1 }");
     }
 }
